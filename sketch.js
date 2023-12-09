be524f3aafe2f59007cade8ef4374963f3236039
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2023-12-09 13:43:56 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 
-
+var cloud, cloudsGroup, cloudImage
 
 
 var score;
@@ -13,7 +13,7 @@ function preload(){
   
   groundImage = loadImage("ground2.png");
   
- 
+ cloudImage = loadImage("cloud.png");
   
 }
 
@@ -73,7 +73,17 @@ function draw() {
 //function to spawn the clouds
 function spawnClouds(){
  // write your code here 
- 
+ if(frameCount % 60 === 0) {
+ cloud = createSprite(600,100,40,10);
+   cloud.addImage(cloudImage)
+   cloud.y = Math.round(random(10,60))
+   cloud.scale = 0.4;
+   cloud.velocityX = -3;
+
+   //adjust the depth
+   cloud.depth = trex.depth
+   trex.depth = trex.depth + 1;
+ }
 }
 
 
